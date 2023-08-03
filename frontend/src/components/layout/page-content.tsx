60f1f32ce214a4e8eff6ca13b592719e7984b7c0
Jason Schrader <whoabuddy@users.noreply.github.com>
2023-08-03 15:53:06 -0700

    feat: Update page content and connect wallet component
    - Update page-content.tsx to include tabs for different sections of the page
    - Add new components for List of Polls, View a Poll, and Create a Poll
    - Update connect-wallet.tsx to display a message and a sign-in component
    
    The changes in this commit update the page content to include tabs for different sections of the page. It also adds new components for managing polls and connecting a wallet. This allows users to easily navigate between different sections and perform actions such as creating a new poll or viewing existing polls.

--- frontend/src/components/layout/page-content.tsx
+++ frontend/src/components/layout/page-content.tsx
@@ -1,29 +1,62 @@
-import { Box, Heading, ListItem, OrderedList, Text } from "@chakra-ui/react";
+import {
+  Box,
+  ListItem,
+  OrderedList,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
+import { useAtomValue } from "jotai";
+import { stxAddressAtom } from "../../constants";
+import ConnectWallet from "../verification-flow/connect-wallet";
 
 function Content() {
+  const stxAddress = useAtomValue(stxAddressAtom);
+
   return (
-    <Box maxW="1200px">
-      <Landing />
+    <Box width="100%" maxW="1200px">
+      {stxAddress ? <Landing /> : <ConnectWallet />}
     </Box>
   );
 }
 
 function Landing() {
   return (
-    <>
-      <Heading pb={8}>polls-nft</Heading>
-      <Text pb={4}>Manage 1btc polls and mint NFTs for participants.</Text>
-      <Text pb={4}>WIP - main goals listed below</Text>
-      <OrderedList ms={8}>
-        <ListItem>Create a new poll</ListItem>
-        <ListItem>Paste poll into console</ListItem>
-        <ListItem>Inscribe image for poll</ListItem>
-        <ListItem>Record poll votes</ListItem>
-        <ListItem>Inscribe poll results</ListItem>
-        <ListItem>Deploy NFT contract 🪂</ListItem>
-        <ListItem>Share results 𝕏 style</ListItem>
-      </OrderedList>
-    </>
+    <Tabs variant="enclosed" colorScheme="orange">
+      <TabList>
+        <Tab>Home</Tab>
+        <Tab>List of Polls</Tab>
+        <Tab>View a Poll</Tab>
+        <Tab>Create a Poll</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel>
+          <Text pb={4}>Manage 1btc polls and mint NFTs for participants.</Text>
+          <Text pb={4}>WIP - main goals listed below</Text>
+          <OrderedList ms={8}>
+            <ListItem>Create a new poll</ListItem>
+            <ListItem>Paste poll into console</ListItem>
+            <ListItem>Inscribe image for poll</ListItem>
+            <ListItem>Record poll votes</ListItem>
+            <ListItem>Inscribe poll results</ListItem>
+            <ListItem>Deploy NFT contract 🪂</ListItem>
+            <ListItem>Share results 𝕏 style</ListItem>
+          </OrderedList>
+        </TabPanel>
+        <TabPanel>
+          <ListPolls />
+        </TabPanel>
+        <TabPanel>
+          <ViewPoll />
+        </TabPanel>
+        <TabPanel>
+          <CreatePoll />
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
   );
 }
 
@@ -33,6 +66,10 @@ function ListPolls() {
 
 function ViewPoll() {
   return <Text>View Poll</Text>;
+}
+
+function CreatePoll() {
+  return <Text>Create Poll</Text>;
 }
 
 export default Content;
--- frontend/src/components/verification-flow/connect-wallet.tsx
+++ frontend/src/components/verification-flow/connect-wallet.tsx
@@ -1,18 +1,4 @@
-import {
-  IconButton,
-  Link,
-  ListItem,
-  Popover,
-  PopoverBody,
-  PopoverCloseButton,
-  PopoverContent,
-  PopoverHeader,
-  PopoverTrigger,
-  Stack,
-  Text,
-  UnorderedList,
-} from "@chakra-ui/react";
-import { FaQuestion } from "react-icons/fa";
+import { Box, Text } from "@chakra-ui/react";
 import SignIn from "../auth/sign-in";
 
 // active step = 0
@@ -20,67 +6,10 @@ import SignIn from "../auth/sign-in";
 
 function ConnectWallet() {
   return (
-    <>
-      <Stack
-        direction="row"
-        justifyContent="space-between"
-        alignItems="center"
-        mb={8}
-      >
-        <Text>
-          Connect your Hiro or Xverse wallet to authenticate into 1btc's
-          Fullcoiner chat.
-        </Text>
-        <Popover placement="bottom-end" variant="1btc-orange">
-          <PopoverTrigger>
-            <IconButton
-              aria-label="Learn More"
-              title="Learn More"
-              icon={<FaQuestion />}
-            />
-          </PopoverTrigger>
-          <PopoverContent>
-            <PopoverHeader pl={4} pt={4}>
-              Connect your Wallet
-            </PopoverHeader>
-            <PopoverCloseButton />
-            <PopoverBody p={4}>
-              <UnorderedList>
-                <ListItem>
-                  To access the 1btc chat platform, connect your Bitcoin wallet
-                  to use with the chat.
-                </ListItem>
-                <ListItem>
-                  We currently support both{" "}
-                  <Link
-                    isExternal
-                    color="orange.500"
-                    href="https://wallet.hiro.so/wallet/install-web"
-                  >
-                    Hiro
-                  </Link>{" "}
-                  and{" "}
-                  <Link
-                    isExternal
-                    color="orange.500"
-                    href="https://www.xverse.app/"
-                  >
-                    Xverse
-                  </Link>{" "}
-                  wallets.
-                </ListItem>
-                <ListItem>
-                  By clicking the Connect Wallet button and selecting your
-                  account from the wallet, you initiate the secure, standardized
-                  connection process.
-                </ListItem>
-              </UnorderedList>
-            </PopoverBody>
-          </PopoverContent>
-        </Popover>
-      </Stack>
+    <Box textAlign="center">
+      <Text mb={8}>Wallet Connection Required</Text>
       <SignIn />
-    </>
+    </Box>
   );
 }
 
